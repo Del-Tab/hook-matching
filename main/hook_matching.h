4f543d3516cedb940af00a345187f7d332a1d3a7
Alban Daumer <daumera@gmail.com>
2020-08-25 10:22:08 +0200

    Update comment about contract for getFrequency

--- main/hook_matching.h
+++ main/hook_matching.h
@@ -126,7 +126,9 @@ uint16_t getMidiNote(const int8_t degree, const uint8_t octave, const struct sca
  * Gets frequencies for a diaonic-like scale
  * to be precise (albeit not exact) we work in floats, caller can round it to int if needed 
   
-  round(getFrequency(NOTE_SIXTE, 5, GAMME_Do, 0)) must return LA4_REF
+  contract for not being exact:
+  when partial part of LA4_REF is not close to 0.5,
+  round(getFrequency(NOTE_SIXTE, 5, GAMME_Do, 0)) must return round(LA4_REF)
 */
 float getFrequency(const int8_t degree, const uint8_t octave,  const struct scale *g, const int8_t transpose) {
   // Getting base do (C) from current octave ... 
