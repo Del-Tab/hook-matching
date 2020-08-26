cd8a3da7c120da2dc4344c39508e5eda8a316214
DelTab <daumera@gmail.com>
2020-08-27 01:28:28 +0200

    implement hooks, scale, sheet, and play function for it  demo with 1st part of traditional old French Tourdion "Quand Je Bois Du Vin Clairet"
    Code needs to be cleaned up but it's late

--- main/hook_matching.h
+++ main/hook_matching.h
@@ -82,18 +82,50 @@ struct scale {
   note note_base;        // the NOTE_something telling which note is the Tonic
   char *display_name;    // a fancy name to display
 };
-/**
-  a sery of notes based on degree
-  not used yet
-*/
+
+
+struct hookpart {
+  int8_t degreeOffset;
+  note_duration duration;
+};
+
 struct scale_hook {
   size_t number;
-  uint8_t *degreeOffsets;
-  int * durations;
-  byte accidentSharps;
-  byte accidentFlats;
-  byte accidentNatural; 
+  struct hookpart part [];
 };
+
+struct contextual_scale_hook {
+  
+  uint8_t octave;
+  uint8_t note;
+  struct scale_hook *hook;
+};
+float getFrequency(const int8_t degree, const uint8_t octave,  struct scale const *g);
+
+
+struct sheet {
+  struct scale * scale;
+  uint16_t bpm; // may ranges from 96 to 480
+  note_duration bpm_unit;
+  uint8_t top;    // number of time unit per "mesure"
+  uint8_t bottom; // if it's 4, blacks are one time unit, if it's 2, white ahre 1 time unit, if it's 8 "croches" are 1 time unit etc
+};
+uint32_t getNoteLengthMillis(note_duration nd, const struct sheet & p) {
+  float base_duration = 60000.0 / (p.bpm_unit * p.bpm);
+  return nd * base_duration;
+}
+
+void play (const int pin, const struct contextual_scale_hook & partie, const struct sheet & p, const struct scale *g) {
+  for (int i = 0; i < partie.hook->number; ++i) {
+    float freq = getFrequency(partie.note - g->note_base + partie.hook->part[i].degreeOffset, partie.octave, g);
+    uint32_t dur = getNoteLengthMillis(partie.hook->part[i].duration, p);
+    tone(pin, round(freq), dur - 25);
+    delay(dur);
+  }
+}
+void play (const int pin, const struct contextual_scale_hook & partie, const struct sheet & p) {
+  play (pin, partie, p, p.scale);
+}
 
 /* return true if the degree in the map is sharp */
 bool isSharp (const struct scale *g, byte degre) {
@@ -109,6 +141,9 @@ bool isFlat (const struct scale *g, byte degre) {
 bool isNatural (const struct scale *g, byte degre) {
   return !(isSharp(g,degre) ^ isFlat(g,degre));
 }
+
+
+
 
 uint16_t getMidiNote(const int8_t degree, const uint8_t octave, const struct scale * g) {
   uint16_t ret = (octave  + hm_div((degree + g->note_base), scaleSize)) * 12 
@@ -128,7 +163,7 @@ uint16_t getMidiNote(const int8_t degree, const uint8_t octave, const struct sca
   
   round(getFrequency(NOTE_SIXTE, 5, GAMME_Do, 0)) must return LA4_REF
 */
-float getFrequency(const int8_t degree, const uint8_t octave,  const struct scale *g, const int8_t transpose) {
+float getFrequency(const int8_t degree, const uint8_t octave, struct scale const *g, const int8_t transpose) {
   // Getting base do (C) from current octave ... 
   int16_t offsetFromLa440 = getMidiNote(degree, octave, g) + transpose;
   offsetFromLa440 -= 5*12 + diatonic_offsets[NOTE_SIXTE];
@@ -140,7 +175,7 @@ float getFrequency(const int8_t degree, const uint8_t octave,  const struct scal
 
 
 
-float getFrequency(int8_t degree, uint8_t octave,  struct scale *g) {
+float getFrequency(int8_t degree, uint8_t octave,  struct scale const *g) {
   return getFrequency(degree, octave, g, 0);
 }
 uint8_t getSensible(const struct scale *scale) {
--- main/main.ino
+++ main/main.ino
@@ -17,13 +17,70 @@ struct scale Jazzy3 = {B101110, 0, NOTE_DO, "jazzy 3"};
 struct scale GAMME_La = {B11001, 0, NOTE_LA, "La majeur"};
 struct scale GAMME_Lam = {0, 0, NOTE_LA, "La mineur"};
 
+// fa et do sont #, note de base = mi
+struct scale miMineur = {B1001,0, NOTE_MI, "mi mineur"};
+
+// gamme de mi mineur, 120 bpm (unité de battement = blanche), rythme = 3/2 (3 temps par mesure, unité dde temps = blanche) 
+struct sheet Tourdion = {&miMineur, 120, 48, 3, 2};
+
+// un hook est une suite de notes, pour l'instant on écrit le nombre de notes du hook,
+// puis un array de couple "offset dans le hook en note sur la portée", "durée logique où la noire vaut 24"
+// je pense qu'on peut faire mieux pour représenter les suites de notes, réflexion en cours
+
+struct scale_hook blanchePointee = { 1, {{0, 72}} };
+struct scale_hook hook1 = { 6, {
+                                {0, 24},
+                                {1, 24},
+                                {2, 24},
+                                {3, 24},
+                                {2, 24},
+                                {1, 24}
+                               }
+                          };
+struct scale_hook hook2 = {7, {
+                               {0, 24},
+                               {1, 24},
+                               {-1, 24},
+                               {0, 48},
+                               {-1, 24},
+                               {-2, 24},
+                               {-3, 48}  
+                              }
+                           };
+struct scale_hook hook3= {6, {
+                                {0, 48},
+                                {2, 48},
+                                {1, 48},
+                                {0, 96},
+                                {-1, 48},
+                                {0, 144}
+                               }
+                          };
+// une suite de hook + portée et note de référence du hook.
+// Ça permet de réutiliser des motifs pour gagner de la  place.
+contextual_scale_hook partition[] ={
+  {5, NOTE_MI, &hook1},
+  {5, NOTE_MI, &blanchePointee},
+  {5, NOTE_FA, &hook1},
+  {5, NOTE_SOL, &hook2},
+  {5, NOTE_MI, &hook1},
+  {5, NOTE_MI, &hook3}
+};
+
 
 void setup() {
+  
+  pinMode(speaker, OUTPUT);
   Serial.begin(9600);
 
+  delay(2000);
+  for (int i = 0; i < sizeof partition / sizeof partition[0]; ++i) {
+    play(speaker, partition[i], Tourdion);
+  }
+  
   delay (2000);
-  struct scale *scales[] = { &GAMME_Do,
-                            &GAMME_Dom,
+  struct scale *scales[] = {// &GAMME_Do,
+                            //&GAMME_Dom,
                           //&GAMME_DesertDom,
                          // &GAMME_JazzDo,
                          // &GAMME_JazzDo2,
@@ -45,16 +102,18 @@ void setup() {
       tone(speaker, round(freq), 250);
       delay(250);
     }
+    /*
     LA4_REF = 460;
     for (int j = -16; j < 16; ++j) {
       
-      float freq = getFrequency(j, 5, *scales[i]);
+      float freq = getFrequency(j, 5, scales[i]);
       Serial.print(freq);
       Serial.print(",");
       tone(speaker, round(freq), 250);
       delay(250);
     }
-  delay(1000);
+    delay(1000);
+    */
   }
 }
 
